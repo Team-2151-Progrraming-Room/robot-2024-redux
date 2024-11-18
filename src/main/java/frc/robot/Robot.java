e0da175664bb048643692bc39a2d7b0bac5a4063
Daniel525183 <ps525183@pths209.org>
2024-11-18 10:15:54 -0600

    Just deleted some unnecessary import lines that I accidently kept with my previous commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,11 +8,6 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-import frc.robot.Constants;
-import frc.robot.Constants.DrivetrainConstants;
-import frc.robot.subsystems.Drivetrain;
-
-
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
